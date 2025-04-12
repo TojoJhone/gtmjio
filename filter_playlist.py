3de77c267bf95b3c62bc20b3a667d8affcf44660
TojoJhone <109429110+TojoJhone@users.noreply.github.com>
2025-04-13 01:49:59 +0530

    filter_playlist.py

---
+++ filter_playlist.py
@@ -0,0 +1,24 @@
+import requests
+
+SOURCE_URL = "https://raw.githubusercontent.com/Esmaeli/m3u/refs/heads/main/mvp.m3u"
+CHANNELS_TO_KEEP = ["STAR GOLD HD", "STAR PLUS HD", "STAR GOLD 2", "ZEE TV HD","AND TV", "ZEE ACTION", "ZEE BOLLYWOOD", "ZEE CINEMA"]
+
+def filter_playlist():
+    response = requests.get(SOURCE_URL)
+    lines = response.text.splitlines()
+    
+    filtered = ["#EXTM3U"]
+    keep = False
+    
+    for i in range(len(lines)):
+        line = lines[i]
+        if line.startswith("#EXTINF"):
+            keep = any(channel.lower() in line.lower() for channel in CHANNELS_TO_KEEP)
+        if keep:
+            filtered.append(line)
+
+    with open("custom_playlist.m3u", "w", encoding="utf-8") as f:
+        f.write("\n".join(filtered))
+
+if __name__ == "__main__":
+    filter_playlist()
